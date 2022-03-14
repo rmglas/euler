bf071202f9701d0a13133cd0542d3b26bd48a83a
rmglas <78153815+rmglas@users.noreply.github.com>
2022-03-14 07:29:05 +0100

    Create separate file for prime number calculation

--- problem_3.py
+++ problem_3.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python3
 
 from timing import time_function
-
-def get_next_prime():
-    '''Calculate prime numbers: return the next prime'''
-    primes = [2, 3, 5, 7, 11, 13, 17, 19] # start with trivial prime numbers
-    for prime in primes:
-        yield prime
-    current = 23
-    while True:
-        current_is_prime = True
-        for prime in primes[1:]:
-            # leave out prime number "2" but always add 2 instead
-            if current % prime == 0:
-                current_is_prime = False
-                break
-        if current_is_prime:
-            primes.append(current)
-            yield current
-        current += 2
+from primes import get_next_prime
 
 def largest_prime_factor(x):
     '''Largest prime factor of given x
